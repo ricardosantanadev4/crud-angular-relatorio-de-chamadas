13b3999ef2d053c27ba69e00b26d18145443fe4f
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2023-01-07 23:36:10 -0300

    update

--- src/app/relatorio/relatorio/relatorio.component.ts
+++ src/app/relatorio/relatorio/relatorio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Relatorio } from 'src/app/model/relatorio';
 import { RelatorioService } from '../services/relatorio.service';
 
@@ -9,10 +10,10 @@ import { RelatorioService } from '../services/relatorio.service';
 })
 export class RelatorioComponent {
   // dataSource: Relatorio[] = [{ data: '07/01/2023', hora: '13:02:57', origem: '5581999999999', destino: '5581888888888', situacao: 'Atendida', duracao: '00:00:04', protocolo: '1673107377.43099' }];
-  dataSource: Relatorio[] = [];
+  dataSource$: Observable<Relatorio[]>;
   displayedColumns = ['data', 'hora', 'origem', 'destino', 'situacao', 'duracao', 'protocolo'];
 
   constructor(private relatorioService: RelatorioService) {
-    this.relatorioService.getRelatorio().subscribe(relatorio => this.dataSource = relatorio);
+    this.dataSource$ = this.relatorioService.getRelatorio();
   }
 }
